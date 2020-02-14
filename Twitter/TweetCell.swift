865a4dcac2fe4d77cf1632155a8cc90b419a7338
Jameel Brice <jameelbrice@gmail.com>
2020-02-13 19:00:39 -0500

    Finally Done

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Jameel Brice on 2/13/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
